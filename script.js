a938aeaa12147638fcb95dedb7960560debd2433
Kamaric112 <107142487+Kamaric112@users.noreply.github.com>
2023-01-05 12:32:54 +0700

    updated new api

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
-const display = document.querySelector("#display")
-const displayHeroText = document.querySelector("#Hero_Title")
-const searchInput = document.querySelector("#searchForm")
-const searchButton = document.querySelector("#store_search_link")
-const categoryGroup = document.querySelector(".categoryGroup")
-const BASE_URL = `https://cs-steam-game-api.herokuapp.com/`
-const nextPageBtn = document.querySelector(".next_page")
+const display = document.querySelector("#display");
+const displayHeroText = document.querySelector("#Hero_Title");
+const searchInput = document.querySelector("#searchForm");
+const searchButton = document.querySelector("#store_search_link");
+const categoryGroup = document.querySelector(".categoryGroup");
+const BASE_URL = `https://steam-api-dot-cs-platform-306304.et.r.appspot.com`;
+const nextPageBtn = document.querySelector(".next_page");
 
-
-let counter = 1
-
+let counter = 1;
 
 searchInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    renderGame("q", `${searchInput.value}`)
+    renderGame("q", `${searchInput.value}`);
   }
-})
+});
 
 searchButton.addEventListener("click", () => {
-  renderGame("q", `${searchInput.value}`)
-})
+  renderGame("q", `${searchInput.value}`);
+});
 
-const getData = async (query, value = "",page) => {
+const getData = async (query, value = "", page) => {
   //fetch function
   try {
-    let url = `${BASE_URL}`
+    let url = `${BASE_URL}`;
     if (query === "features") {
-      url = `${BASE_URL}features`
+      url = `${BASE_URL}/features`;
     } else {
-      url = `${BASE_URL}games?${query}=${value}&page=${page}`
+      url = `${BASE_URL}/games?${query}=${value}&page=${page}`;
     }
 
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data)
-    return data
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    return data;
   } catch (error) {
-    console.log("error", error)
+    console.log("error", error);
   }
-}
+};
 
-const renderGame = async (query, value,page) => {
-  display.innerHTML = `<div class="infinity-1"></div>`
+const renderGame = async (query, value, page) => {
+  display.innerHTML = `<div class="infinity-1"></div>`;
 
-  const res = await getData(query, value,page);
-  display.innerHTML = ""
+  const res = await getData(query, value, page);
+  display.innerHTML = "";
 
-  console.log(res)
+  console.log(res);
   res.data.forEach((game) => {
-    const newDiv = document.createElement("div")
+    const newDiv = document.createElement("div");
     if (game["price"] === 0) {
       newDiv.innerHTML = `<div class="game_wrapper">
             <div class="cover" onClick="appDetail(${game["appid"]})">
@@ -58,7 +56,7 @@ const renderGame = async (query, value,page) => {
             <p>FREE</p>
             </div>
             </div>
-            </div>`
+            </div>`;
     } else {
       newDiv.innerHTML = `<div class="game_wrapper">
             <div class="cover" onClick="appDetail(${game["appid"]})">
@@ -68,48 +66,44 @@ const renderGame = async (query, value,page) => {
             <p>$${game["price"]}</p>
             </div>
             </div>
-            </div>`
+            </div>`;
     }
 
     display.appendChild(newDiv);
-  })
-}
+  });
+};
 
 const getGenre = async () => {
   //fetch function
   try {
-    url = `https://cs-steam-game-api.herokuapp.com/genres`;
-    const res = await fetch(url)
-    const data1 = await res.json()
-    console.log(data1)
-    return data1
+    url = `${BASE_URL}/genres`;
+    const res = await fetch(url);
+    const data1 = await res.json();
+    console.log(data1);
+    return data1;
   } catch (error) {
-    console.log("error", error)
+    console.log("error", error);
   }
-}
+};
 const renderGenre = async () => {
   try {
-    const res = await getGenre()
-    const data = res["data"]
-    console.log(data)
+    const res = await getGenre();
+    const data = res["data"];
+    console.log(data);
     data.forEach((genre) => {
-      const x = document.createElement("li")
-      x.innerHTML = `${genre.name}`
-      x.classList.add("genre")
+      const x = document.createElement("li");
+      x.innerHTML = `${genre.name}`;
+      x.classList.add("genre");
       x.addEventListener("click", () => {
-        displayHeroText.innerHTML = `${genre.name.toUpperCase()} GAMES`
-        renderGame("genres", `${genre.name}`)
-       
-      } 
-      )
-      categoryGroup.appendChild(x)
-      
-      })
+        displayHeroText.innerHTML = `${genre.name.toUpperCase()} GAMES`;
+        renderGame("genres", `${genre.name}`);
+      });
+      categoryGroup.appendChild(x);
+    });
   } catch (error) {
-    console.log("error", error)
+    console.log("error", error);
   }
-}
-
+};
 
 //game detail when clicked
 
@@ -117,24 +111,24 @@ const getSpecificGame = async (value) => {
   //fetch function
 
   try {
-    url = `https://cs-steam-game-api.herokuapp.com/single-game/${value}`
-    const res = await fetch(url)
-    const data1 = await res.json()
-    console.log(data1)
-    return data1
+    url = `${BASE_URL}/single-game/${value}`;
+    const res = await fetch(url);
+    const data1 = await res.json();
+    console.log(data1);
+    return data1;
   } catch (error) {
-    console.log("error", error)
+    console.log("error", error);
   }
-}
+};
 
 const appDetail = async (value) => {
-  display.innerHTML = `<div class="infinity-1"></div>`
-  const res = await getSpecificGame(value)
-  const data = await res["data"]
-  displayHeroText.innerHTML = `${data.name}`
-  display.innerHTML = ""
-  console.log(data)
-  const newdiv = document.createElement("div")
+  display.innerHTML = `<div class="infinity-1"></div>`;
+  const res = await getSpecificGame(value);
+  const data = await res["data"];
+  displayHeroText.innerHTML = `${data.name}`;
+  display.innerHTML = "";
+  console.log(data);
+  const newdiv = document.createElement("div");
   newdiv.innerHTML = `<div class="game_detail">
     <div class="title_contain">
       <div class="title">${data.name}</div>
@@ -150,7 +144,10 @@ const appDetail = async (value) => {
           ${data.description}.
         </div>
         <div class="detail_info">
-          <div class="release_date">Release Date: ${data.release_date.slice(0,9)}</div>
+          <div class="release_date">Release Date: ${data.release_date.slice(
+            0,
+            9
+          )}</div>
           <div class="developer">Developer: ${data.developer}</div>
           <div class="publisher">Publisher: ${data.developer}</div>
           <div class="review"> Positive Ratings: ${data.positive_ratings}</div>
@@ -169,43 +166,46 @@ const appDetail = async (value) => {
         <a href="#">${data.genres[2]}</a>
       </div>
     </div>
-  </div>`
+  </div>`;
 
-  display.appendChild(newdiv)
-  const tagBtn = document.querySelector(".tag")
+  display.appendChild(newdiv);
+  const tagBtn = document.querySelector(".tag");
   tagBtn.addEventListener("click", () => {
-    renderGame("genres", `${data.genres}`)
-  })
-}
+    renderGame("genres", `${data.genres}`);
+  });
+};
 
 //Get the return to top button
-const mybutton = document.getElementById("myBtn")
+const mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-  scrollFunction()
-}
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block"
+    mybutton.style.display = "block";
   } else {
-    mybutton.style.display = "none"
+    mybutton.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0
-  document.documentElement.scrollTop = 0
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 // first load
-renderGenre()
-renderGame("features", "")
+renderGenre();
+renderGame("features", "");
 
-
-nextPageBtn.addEventListener("click",() => {
-  counter++
-  renderGame("genres",`${categoryGroup.children[2].textContent}`,`${counter}`)
-})
+nextPageBtn.addEventListener("click", () => {
+  counter++;
+  renderGame(
+    "genres",
+    `${categoryGroup.children[2].textContent}`,
+    `${counter}`
+  );
+});
